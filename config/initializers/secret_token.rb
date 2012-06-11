be393e92a5ea47bc9fdd7113af17496186786e08
Chris Sell <christophersell3@gmail.com>
2012-06-10 21:08:08 -0500

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workfun::Application.config.secret_token = '0d67c270d4e01534e9f758e4af5246a4da1bde373c2c1004839f188708e23f8a0db3f6576abb2127daf7fa7b292cd5d2285158e7ef6b1d6532cd12abc0480f8c'
